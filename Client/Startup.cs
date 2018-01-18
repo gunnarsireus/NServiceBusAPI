4fc8884d5dc3fa25cb8218ba8530472a2deb2502
KONGUNS <KONGUNS@CLN10907>
2018-01-18 07:20:13 +0100

    First

---
+++ Client/Startup.cs
@@ -0,0 +1,101 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NServiceBus;
+using Client.DAL;
+using Client.Services;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Client.Models;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Client
+{
+	public class Startup
+	{
+		IEndpointInstance EndpointInstance { get; set; }
+
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			var endpointConfiguration = new EndpointConfiguration("NServiceBusCore.Client");
+			var transport = endpointConfiguration.UseTransport<LearningTransport>();
+			endpointConfiguration.UsePersistence<LearningPersistence>();
+			endpointConfiguration.MakeInstanceUniquelyAddressable("1");
+			endpointConfiguration.EnableCallbacks();
+
+			EndpointInstance = Endpoint.Start(endpointConfiguration).GetAwaiter().GetResult();
+
+			services.AddSingleton(EndpointInstance);
+
+			services.AddIdentity<ApplicationUser, IdentityRole>()
+				.AddEntityFrameworkStores<ApplicationDbContext>()
+				.AddDefaultTokenProviders();
+
+			//// Add application services.
+			services.AddTransient<IEmailSender, EmailSender>();
+
+			services.AddMvc();
+
+			var task = ConfigureServicesAsync(services);
+
+			task.Wait();
+
+		}
+
+		async Task ConfigureServicesAsync(IServiceCollection services)
+		{
+			string aspNetDb = null;
+			var aspNetDbLocation = new AspNetDbLocation();
+			try
+			{
+				var getAspNetDb = await aspNetDbLocation.GetAspNetDbAsync(EndpointInstance);
+				aspNetDb = getAspNetDb.AspNetDb;
+			}
+			catch (Exception e)
+			{
+				//Do nothing
+			}
+			if (aspNetDb != null)
+			{
+				services.AddDbContext<ApplicationDbContext>(options =>
+					options.UseSqlite("Data Source=" + aspNetDb));
+			}
+			else
+			{
+				services.AddDbContext<ApplicationDbContext>(options =>
+					options.UseSqlite("Data Source=" + Directory.GetCurrentDirectory() + "\\App_Data\\AspNet.db"));
+			}
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+		{
+			loggerFactory.AddDebug();
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+				app.UseBrowserLink();
+				app.UseDatabaseErrorPage();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Home/Error");
+			}
+
+			app.UseStaticFiles();
+
+			app.UseAuthentication();
+
+			app.UseMvc(routes =>
+			{
+				routes.MapRoute(
+					"default",
+					"{controller=Home}/{action=Index}/{id?}");
+			});
+		}
+	}
+}
---
+++ Server/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System.Globalization;
+using Microsoft.AspNetCore;
+using Server.Extensions;
+using System.IO;
+
+namespace Server
+{
+
+	internal class Program
+	{
+		public static void Main(string[] args)
+		{
+			CultureInfo.CurrentUICulture = new CultureInfo("en-US");
+			BuildWebHost(args).Run();
+		}
+
+		static IWebHost BuildWebHost(string[] args) =>
+			WebHost.CreateDefaultBuilder(args)
+				.UseKestrel()
+				.UseContentRoot(Directory.GetCurrentDirectory())
+				.UseIISIntegration()
+				.UseStartup<Startup>()
+				.InitSqLiteDb("DataSource=App_Data/Car.db")
+				.Build();
+	}
+
+}
