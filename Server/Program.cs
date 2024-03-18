ed27debd92c74e482f3cd358c043c86f79152e0b
Gunnar Siréus <gunnar.sireus@gmail.com>
2024-03-18 08:02:44 +0100

    Refactoring

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -17,111 +18,108 @@ using System.Threading.Tasks;
 
 namespace Server
 {
-  internal class Program
-  {
-    public static async Task Main(string[] args)
+    internal class Program
     {
-      CultureInfo.CurrentUICulture = new CultureInfo("en-US");
-      var builder = new ConfigurationBuilder()
-        .AddJsonFile("appsettings.json");
+        public static async Task Main(string[] args)
+        {
+            CultureInfo.CurrentUICulture = new CultureInfo("en-US");
+            var builder = new ConfigurationBuilder()
+              .AddJsonFile("appsettings.json");
 
-      var configuration = builder.Build();
-      var app = CreateHostBuilder(args, configuration).Build();
+            var configuration = builder.Build();
+            var host = CreateHostBuilder(args, configuration).Build();
 
-            using (var scope = app.Services.GetService<IServiceScopeFactory>().CreateScope())
+            using (var scope = host.Services.CreateScope())
             {
                 var carapicontext = scope.ServiceProvider.GetRequiredService<CarApiContext>();
                 carapicontext.EnsureSeedData();
 
-                var database = scope.ServiceProvider.GetRequiredService<NServiceBusDbContext>().Database;
-
-                var sql = $"delete from [NServiceBusDb].[dbo].[error]";
-                await database.ExecuteSqlRawAsync(sql);
-
-                sql = $"delete from [NServiceBusDb].[dbo].[audit]";
-                await database.ExecuteSqlRawAsync(sql);
-
-                sql = $"delete from [NServiceBusDb].[dbo].[NServiceBusCore.Client.Delayed]";
-                await database.ExecuteSqlRawAsync(sql);
-
-                sql = $"delete from [NServiceBusDb].[dbo].[NServiceBusCore.Server.Delayed]";
-                await database.ExecuteSqlRawAsync(sql);
-
-                sql = $"delete from [NServiceBusDb].[dbo].[NServiceBusCore.Client]";
-                await database.ExecuteSqlRawAsync(sql);
-
-                sql = $"delete from [NServiceBusDb].[dbo].[NServiceBusCore.Client.1]";
-                await database.ExecuteSqlRawAsync(sql);
-
-                sql = $"delete from [NServiceBusDb].[dbo].[NServiceBusCore.Server]";
-                await database.ExecuteSqlRawAsync(sql);
-
-                sql = $"delete from [NServiceBusDb].[dbo].[NServiceBusCore.Server.1]";
-                await database.ExecuteSqlRawAsync(sql);
-
-                sql = $"delete from [NServiceBusDb].[dbo].[SubscriptionRouting]";
-                await database.ExecuteSqlRawAsync(sql);
+                var nServiceBusDbContext = scope.ServiceProvider.GetRequiredService<NServiceBusDbContext>();
+                var database = nServiceBusDbContext.Database;
+                await ClearDatabaseTables(database);
             }
 
-            await app.RunAsync();
+            await host.RunAsync();
+        }
+
+        static async Task ClearDatabaseTables(DatabaseFacade database)
+        {
+            var tables = new[]
+            {
+                "error",
+                "audit",
+                "NServiceBusCore.Client.Delayed",
+                "NServiceBusCore.Server.Delayed",
+                "NServiceBusCore.Client",
+                "NServiceBusCore.Client.1",
+                "NServiceBusCore.Server",
+                "NServiceBusCore.Server.1",
+                "SubscriptionRouting"
+            };
+
+            foreach (var table in tables)
+            {
+                var sql = $"delete from [NServiceBusDb].[dbo].[{table}]";
+                await database.ExecuteSqlRawAsync(sql);
+            }
         }
 
         static IHostBuilder CreateHostBuilder(string[] args, IConfiguration configuration)
-    {
-      return Host.CreateDefaultBuilder(args)
-          .ConfigureLogging(logging =>
-          {
-            logging.AddConsole();
-          })
-         .UseNServiceBus(ctx =>
-         {
-           string endpointName = "NServiceBusCore.Server";
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(logging =>
+                {
+                    logging.AddConsole();
+                })
+               .UseNServiceBus(ctx =>
+               {
+                   string endpointName = "NServiceBusCore.Server";
 
-           var endpointConfiguration = new EndpointConfiguration(endpointName);
+                   var endpointConfiguration = new EndpointConfiguration(endpointName);
 
-           endpointConfiguration.ApplyEndpointConfiguration(
-                     ctx.Configuration.GetConnectionString("NServiceBusTransport"),
-                     EndpointMappings.MessageEndpointMappings());
+                   endpointConfiguration.ApplyEndpointConfiguration(
+                       ctx.Configuration.GetConnectionString("NServiceBusTransport"),
+                       EndpointMappings.MessageEndpointMappings());
 
-           endpointConfiguration.RegisterComponents(registration =>
-           {
-             registration.AddTransient<CreateCarRequestHandler>();
-             registration.AddTransient<CreateCompanyRequestHandler>();
-             registration.AddTransient<DeleteCarRequestHandler>();
-             registration.AddTransient<DeleteCompanyRequestHandler>();
-             registration.AddTransient<GetCarRequestHandler>();
-             registration.AddTransient<GetCarsRequestHandler>();
-             registration.AddTransient<GetCompanyRequestHandler>();
-             registration.AddTransient<GetCompaniesRequestHandler>();
-             registration.AddTransient<UpdateCarRequestHandler>();
-             registration.AddTransient<UpdateCompanyRequestHandler>();
+                   endpointConfiguration.RegisterComponents(registration =>
+             {
+                     registration.AddTransient<CreateCarRequestHandler>();
+                     registration.AddTransient<CreateCompanyRequestHandler>();
+                     registration.AddTransient<DeleteCarRequestHandler>();
+                     registration.AddTransient<DeleteCompanyRequestHandler>();
+                     registration.AddTransient<GetCarRequestHandler>();
+                     registration.AddTransient<GetCarsRequestHandler>();
+                     registration.AddTransient<GetCompanyRequestHandler>();
+                     registration.AddTransient<GetCompaniesRequestHandler>();
+                     registration.AddTransient<UpdateCarRequestHandler>();
+                     registration.AddTransient<UpdateCompanyRequestHandler>();
 
-             registration.AddTransient<CreateCarResponseHandler>();
-             registration.AddTransient<CreateCompanyResponseHandler>();
-             registration.AddTransient<DeleteCarResponseHandler>();
-             registration.AddTransient<DeleteCompanyResponseHandler>();
-             registration.AddTransient<GetCarResponseHandler>();
-             registration.AddTransient<GetCarsResponseHandler>();
-             registration.AddTransient<GetCompanyResponseHandler>();
-             registration.AddTransient<GetCompaniesResponseHandler>();
-             registration.AddTransient<UpdateCarResponseHandler>();
-             registration.AddTransient<UpdateCompanyResponseHandler>();
-           });
+                     registration.AddTransient<CreateCarResponseHandler>();
+                     registration.AddTransient<CreateCompanyResponseHandler>();
+                     registration.AddTransient<DeleteCarResponseHandler>();
+                     registration.AddTransient<DeleteCompanyResponseHandler>();
+                     registration.AddTransient<GetCarResponseHandler>();
+                     registration.AddTransient<GetCarsResponseHandler>();
+                     registration.AddTransient<GetCompanyResponseHandler>();
+                     registration.AddTransient<GetCompaniesResponseHandler>();
+                     registration.AddTransient<UpdateCarResponseHandler>();
+                     registration.AddTransient<UpdateCompanyResponseHandler>();
+                 });
 
-           return endpointConfiguration;
-         })
-         .ConfigureServices(services =>
-         {
-           services.AddDbContext<CarApiContext>(options =>
-               options.UseSqlServer(configuration.GetConnectionString("CarApiConnection")));
+                   return endpointConfiguration;
+               })
+               .ConfigureServices(services =>
+               {
+                   services.AddDbContext<CarApiContext>(options =>
+                 options.UseSqlServer(configuration.GetConnectionString("CarApiConnection")));
 
-             services.AddDbContext<NServiceBusDbContext>(options =>
-                    options.UseSqlServer(configuration.GetConnectionString("NServiceBusTransport")));
+                   services.AddDbContext<NServiceBusDbContext>(options =>
+                          options.UseSqlServer(configuration.GetConnectionString("NServiceBusTransport")));
 
-             services.AddTransient<ICarRepository, CarRepository>();
-           services.AddTransient<ICompanyRepository, CompanyRepository>();
-         });
+                   services.AddTransient<ICarRepository, CarRepository>();
+                   services.AddTransient<ICompanyRepository, CompanyRepository>();
+               });
+        }
     }
-  }
 }
 
